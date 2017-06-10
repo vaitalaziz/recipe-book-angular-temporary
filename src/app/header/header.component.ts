51ded67bb7bd20ee691cca69604422fcb518c79a
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-06-10 17:35:41 +0200

    deleted EventEmitter & onSelect(), no need

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,10 +8,10 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent{
 
-    @Output() featureSelected = new EventEmitter<string>();
-    onSelect(feature: string) {
-        this.featureSelected.emit(feature);
-    }
+    // @Output() featureSelected = new EventEmitter<string>();
+    // onSelect(feature: string) {
+    //     this.featureSelected.emit(feature);
+    // }
       
    
 }
