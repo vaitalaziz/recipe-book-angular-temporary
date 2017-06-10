0535160b6dc47fb20aa542f17179b3883616aaca
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-06-10 04:22:24 +0200

    imports AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service'; 
+import { AppRoutingModule } from './app-routing.module'; 
 
 
 @NgModule({
@@ -33,7 +34,8 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
