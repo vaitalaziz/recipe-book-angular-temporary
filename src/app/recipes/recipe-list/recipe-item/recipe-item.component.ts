80206a40325371a86132fd72e4a9944a1561546a
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-06-11 14:40:04 +0200

    'index' will be setup from 'recipe-list'

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -12,14 +12,15 @@ export class RecipeItemComponent implements OnInit {
 
   // listening recipe from 'recipe-list'
   @Input() recipe: Recipe;  
+  @Input() index: number; // index will listen to setup from 'recipe-list'
 
 
-  constructor(private recipeService: RecipeService){}
+ // constructor(private recipeService: RecipeService){}
   
-  onSelected(){
-    // EventEmitter dicleared at recipe service & emit here but output/subscribe of this at "recipes.component.ts"
-    this.recipeService.recipeSelected.emit(this.recipe); 
-  }
+  // onSelected(){
+  //   // EventEmitter dicleared at recipe service & emit here but output/subscribe of this at "recipes.component.ts"
+  //   this.recipeService.recipeSelected.emit(this.recipe); 
+  // }
   ngOnInit() {
   }
 
