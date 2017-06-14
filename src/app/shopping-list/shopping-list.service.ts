264279bcb0e2e778594dc417e1010014f0dcbec2
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-06-14 19:54:23 +0200

    Subject instead of EventEmitter

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,11 @@
 import { EventEmitter } from '@angular/core'; 
 import { Ingredient } from '../shared/ingredient.model';  
+import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
-   ingredientsChanged = new EventEmitter<Ingredient[]>();
+   //ingredientsChanged = new EventEmitter<Ingredient[]>();
+   
+    ingredientsChanged = new Subject<Ingredient[]>();
    
    private ingredients: Ingredient[] = [
     new Ingredient('Apple',  3),
@@ -16,7 +19,7 @@ export class ShoppingListService {
   addIngredient(ingredient: Ingredient){
       this.ingredients.push(ingredient);
 // very important **  need emit to have always get the update ingredients array after adding/deleting....
-      this.ingredientsChanged.emit(this.ingredients.slice()); 
+      this.ingredientsChanged.next(this.ingredients.slice()); 
   }
   addIngredients(ingredients: Ingredient[]){
     //   for (let ingredient of ingredients) {
@@ -27,6 +30,6 @@ export class ShoppingListService {
      // ... ingredients into list of single ingredients 
       this.ingredients.push(...ingredients);
      // all ingredients one go & then emit  
-      this.ingredientsChanged.emit(this.ingredients.slice()); 
+      this.ingredientsChanged.next(this.ingredients.slice()); 
   }
 }
