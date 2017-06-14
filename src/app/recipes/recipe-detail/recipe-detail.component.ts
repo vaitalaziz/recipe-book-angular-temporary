6afb9c26d2a6c54b0d26c9e2d4602b59a1ebf809
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-06-14 19:39:05 +0200

    onEditRecipe() added

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router'; 
+import { ActivatedRoute, Params, Router } from '@angular/router'; 
 
 import {Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service'; 
@@ -18,7 +18,8 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
 
   constructor(private recipeService: RecipeService, 
-                private route: ActivatedRoute) { }
+                private route: ActivatedRoute,
+                private router: Router) { }
 
   ngOnInit() {
     // const id = this.route.snapshot.params['id'];
@@ -33,5 +34,10 @@ export class RecipeDetailComponent implements OnInit {
     // through recipe service 
     // in recipe service inject shopping-list servie to emit below code
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+  onEditRecipe(){
+    // for complex route path way:  
+      // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route}); 
+     this.router.navigate(['edit'], {relativeTo: this.route}); 
   }
 }
