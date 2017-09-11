f42a2a053de172ef555ebb2e2bf036f858d52d10
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-09-11 17:37:50 +0200

    just changed recipe with new 'name, image'

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -17,16 +17,16 @@ export class RecipeService {
         new Ingredient('Meat', 2),
         new Ingredient('French Fries', 15)
       ]),
-    new Recipe('Chapati', 
-      'Chapati....testy', 
-      'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg',
+    new Recipe('Ilish Fish', 
+      'King of the Fish....testy', 
+      'https://3.bp.blogspot.com/-5D_1zKcavFo/VtFMfFuD2mI/AAAAAAAAMBc/ALMSy0tVbn4/s1600/Bhapa%2BShorshe%2BIlish%2B5.JPG',
       [
         new Ingredient('Veg', 4),
         new Ingredient('Juice', 1)
       ]),
-    new Recipe('Shorma', 
-      'Shorma...awesome', 
-      'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg',
+    new Recipe('Chicken Biriani', 
+      'yahooo...awesome', 
+      'http://rupcare.com/wp-content/uploads/2016/09/rupcare_kacchi-biryani-recipe.jpg',
       [
         new Ingredient('Salad', 1),
         new Ingredient('Tea', 3)
