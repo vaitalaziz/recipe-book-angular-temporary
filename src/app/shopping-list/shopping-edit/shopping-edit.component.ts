6b6c65db5c6cf77d5d95591cbfc18d270e4b1955
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-09-12 10:43:48 +0200

    read comments & update function to edit or add new ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -15,10 +15,12 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   // no need becauase we don't use local ref so
   // @ViewChild('nameInput') nameInputRef: ElementRef;
   // @ViewChild('amountInput') amountInputRef: ElementRef;
-  
+  @ViewChild('f') slForm: NgForm;
+  // subscribe edit component need to unsubscribe to prevent memory waste, so need Subscription
   subscription: Subscription; 
   editMode = false;
   editItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private slistService: ShoppingListService) { }
 
@@ -28,19 +30,35 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         (index: number) => {
           this.editItemIndex = index;
           this.editMode = true;
+          this.editedItem = this.slistService.getIngredient(index); 
+          // after retrieving all info need to set new value in that form 
+          this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          })
+
         }
       ); 
   }
 
-  onAddItem(form: NgForm){ // const used bcz no planning to change so, otherwise 'let' use
+  onUpdateOrAddItem(form: NgForm){ // const used bcz no planning to change so, otherwise 'let' use
   // no need becauase we odn't use local ref so
     // const ingName = this.nameInputRef.nativeElement.value;
     // const ingAmount = this.amountInputRef.nativeElement.value;
     // const newIngredient = new Ingredient(ingName, ingAmount);
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
+    // checking editMode 
+    if (this.editMode){
+      this.slistService.updateIngredient(this.editItemIndex, newIngredient)
+    } // or adding brand new ingredient 
+    else {
+      this.slistService.addIngredient(newIngredient); 
+    }
+    this.editMode = false; // for Update button 
+    form.reset();
     //
-    this.slistService.addIngredient(newIngredient); 
+    
      
   }
 
