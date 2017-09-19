be53500621f49b1dfcb18660638247870e970523
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-09-19 11:45:17 +0200

    added route to new & edit recipe path for authenticated user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,15 +8,16 @@ import { RecipeStartComponent} from './recipes/recipe-start/recipe-start.compone
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component'; 
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component'; 
 import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component'
+import { SigninComponent } from './auth/signin/signin.component';
+import { AuthGuard } from './auth/auth-guard.service';
 // all routes 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full'},
     { path: 'recipes', component: RecipesComponent, children: [
         {path: '', component: RecipeStartComponent },
-        {path: 'new', component: RecipeEditComponent}, // 'new' would be before of 'id'
+        {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard]}, // 'new' would be before of 'id'
         {path: ':id', component: RecipeDetailComponent},
-        {path: ':id/edit', component: RecipeEditComponent},
+        {path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard]},
     ] },
     { path: 'shopping-list', component: ShoppingListComponent},
     { path: 'signup', component: SignupComponent},
