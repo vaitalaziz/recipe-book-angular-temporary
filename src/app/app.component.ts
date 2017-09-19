0772c4677d07049f9f7e0e352d0a56edf7950e34
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-09-19 09:16:33 +0200

    added firebase initialization purpose

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // //loadedFeature = 'recipe';
   
   // onNavigate(feature: string){
   //   //  this.loadedFeature = feature;
   // }
+
+// at app starting time need to start firebase SDK so best place to initialize firebase here 
+ngOnInit(){
+  firebase.initializeApp({
+    // these key value pairs got from firebase 'WEB SETUP' 
+    apiKey: "AIzaSyCA_VqWFQLMcAjjCvO-Ngdu3Vuf1a3JJao",
+    authDomain: "aziz-ng-recipe-book.firebaseapp.com"
+  });
+}
+
 }
 
 
