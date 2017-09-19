7cdde297463c7cb8a92cf8766c06e303caf1c761
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-09-19 11:47:29 +0200

    injecting Router, update signinUser(), added logout() & look comments

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,15 @@
 
+import { Router } from '@angular/router';
 import * as firebase from 'firebase';
+import { Injectable } from '@angular/core'
 
+@Injectable()
 export class AuthService{
     token: string;
 
+    constructor(private router: Router){
+
+    }
     signupUser(email: string, password: string){
         // builtin firebase SDK 'auth' & 'createUserWithEmailAndPassword' 
         firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -17,6 +23,7 @@ export class AuthService{
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
                 response => {
+                    this.router.navigate(['/']); // after login success redirect to page
                     //console.log(response)
                     // after login sending requesting to firebase to get token
                     firebase.auth().currentUser.getIdToken()
@@ -40,5 +47,14 @@ export class AuthService{
           );
           return this.token; // it is dangerous to return token, becuase if expired token by timeout or refresing for, return then user can't use as needed. 
           // here just focusing on frontend Angular so just simple purpose using it. 
+    }
+
+    isAuthenticatedUser(){
+        return this.token !=null; // if not null mean the token valid 
+    }
+
+    logout(){
+        firebase.auth().signOut();
+        this.token = null; 
     }
 }
