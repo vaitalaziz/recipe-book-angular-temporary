14f2a498f907f0c245feef7f36b5dc94eba728c6
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-09-19 10:41:06 +0200

    added 'getToken()', updated 'signinUser()' for authenticated user

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,8 @@
 import * as firebase from 'firebase';
 
 export class AuthService{
+    token: string;
+
     signupUser(email: string, password: string){
         // builtin firebase SDK 'auth' & 'createUserWithEmailAndPassword' 
         firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -14,10 +16,29 @@ export class AuthService{
         // builtin firebase SDK 'auth' & 'createUserWithEmailAndPassword'
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
-                response => console.log(response)
+                response => {
+                    //console.log(response)
+                    // after login sending requesting to firebase to get token
+                    firebase.auth().currentUser.getIdToken()
+                        .then( // then waiting for token to assign it into local variable token
+                            (token: string) => this.token = token
+                        )
+                }
             )
             .catch(
                 error => console.log(error)
             );
+    }
+
+    getToken(){
+      // asyncronizely firebase retrieve token from local stroage and if local stroage token is invalid or time out then 
+        // firebase will give token again, so asyncronizely works, so works as promise
+      // firebase SDK giving back all method here 
+        firebase.auth().currentUser.getIdToken()
+            .then( // then waiting for token to assign it into local variable token
+                 (token: string) => this.token = token
+          );
+          return this.token; // it is dangerous to return token, becuase if expired token by timeout or refresing for, return then user can't use as needed. 
+          // here just focusing on frontend Angular so just simple purpose using it. 
     }
 }
