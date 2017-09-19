2eab22a5a8cdffab50be316e4386fd9d2fade131
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-09-19 10:43:04 +0200

    updated 'getRecipes()', 'storageRecipes()' for authorize user

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { AuthService } from '../auth/auth.service'
 
 import 'rxjs/Rx';
 import { RecipeService } from '../recipes/recipe.service'; 
@@ -8,22 +9,28 @@ import { Recipe } from '../recipes/recipe.model'
 
 @Injectable()
 export class DataStroageService{
-    constructor(private http: Http, private recipeService: RecipeService){}
+    constructor(private http: Http, private recipeService: RecipeService,
+                private authService: AuthService){}
     
     storageRecipes(){
+        const token = this.authService.getToken();
         // here 'put' request for firebase, it depends on base on my backend 
         // getRecipes method will bring recipes list and pass to put method to save into firebase
         // returning observable
-       return  this.http.put('https://aziz-ng-recipe-book.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+       return  this.http.put('https://aziz-ng-recipe-book.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes(){
-        this.http.get('https://aziz-ng-recipe-book.firebaseio.com/recipes.json')
+        const token = this.authService.getToken();
+        // query parameter = ?auth=' + token
+        this.http.get('https://aziz-ng-recipe-book.firebaseio.com/recipes.json?auth=' + token)
             .map(
                 (response: Response) => {
-                    const recipes: Recipe[] = response.json(); // fetching recipe from firebase & json format convert into js.
+                    // fetching recipe from firebase & json format convert into js.
+                    const recipes: Recipe[] = response.json(); 
                     for (let recipe of recipes) {
-                        if (!recipe['ingredients']){ // if recipe['ingredients'] doesn't exist then will be added as empty property, during fetching from firebase 
+                        // if recipe['ingredients'] doesn't exist then will be added as empty property, during fetching from firebase
+                        if (!recipe['ingredients']){  
                         //console.log(recipe); 
                             recipe['ingredients'] = [];
                         }
