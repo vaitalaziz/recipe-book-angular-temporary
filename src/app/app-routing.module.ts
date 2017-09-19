86fb20a5eb3f5943fe66d0413c2e6acae8621f66
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-09-19 09:19:16 +0200

    fixing routing path for 'signup', & 'signin'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent} from './recipes/recipe-start/recipe-start.component'; 
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component'; 
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component'; 
-
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component'
 // all routes 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full'},
@@ -18,6 +19,8 @@ const appRoutes: Routes = [
         {path: ':id/edit', component: RecipeEditComponent},
     ] },
     { path: 'shopping-list', component: ShoppingListComponent},
+    { path: 'signup', component: SignupComponent},
+    { path: 'signin', component: SigninComponent}
 ]; 
 
 // need to do it
